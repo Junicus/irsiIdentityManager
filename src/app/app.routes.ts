8b2b33d0d19934cfcaf1849518e2e5a4de34e3ed
Nelson Segarra <junicus@outlook.com>
2017-02-13 16:26:08 -0400

    home route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,8 +2,10 @@ import { ModuleWithProviders } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { UsersListComponent } from './identity-manager/users/users-list.component';
 import { RolesListComponent } from './identity-manager/roles/roles-list.component';
+import { HomeComponent } from './identity-manager/home/home.component';
 
 export const routes: Routes = [
+  { path: '', component: HomeComponent },
   { path: 'users', component: UsersListComponent },
   { path: 'roles', component: RolesListComponent }
 ];
